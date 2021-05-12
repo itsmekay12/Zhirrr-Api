063db1c6a1b51d8433b4bf0d9c9998f171ec8e67
itskayX404 <81577355+itskayX404@users.noreply.github.com>
2021-05-13 02:13:14 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2806,7 +2806,7 @@ router.get('/maker/special/epep', async (req, res, next) => {
 })
 
 router.get('/cekapikey', async(req, res, next) => {
-  var apikeyImput = req.query.apikey;
+  var apikeyInput = req.query.apikey;
   if(!apikey) return res.json(loghandler.notparam)
   if(apikeyInput != 'kawaiapi') return res.json(loghandler.invalidKey)
     res.json({
